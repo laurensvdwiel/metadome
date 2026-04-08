7f76e07f507fb7f09e06504939483daa014a612a
laurensvdwiel <laurensvdwiel@gmail.com>
2026-04-08 12:04:35 -0700

    revised external_links.js to reduce redundancy and usage throughout the website. Removed other double loading scripts in index.html and ensure single DOM check

--- metadome/presentation/web/static/js/metadome/external_links.js
+++ metadome/presentation/web/static/js/metadome/external_links.js
@@ -43,6 +43,86 @@ function normalizeChromosome(chromosome) {
     return null;
 }
 
+const STATIC_EXTERNAL_LINKS = {
+    MetaDome: {
+        app: "https://www.metadome.app",
+        legacy: "https://stuart.radboudumc.nl/metadome/",
+        github: "https://github.com/laurensvdwiel/metadome",
+        githubReleases: "https://github.com/laurensvdwiel/metadome/releases",
+        githubIssues: "https://github.com/laurensvdwiel/metadome/issues",
+        zenodoV1: "https://zenodo.org/records/6625251",
+        zenodoV2: "https://zenodo.org/records/19376150"
+    },
+    "MetaDome.papers": {
+        humu2017: "https://doi.org/10.1002/humu.23313",
+        humu2019: "https://doi.org/10.1002/humu.23798",
+        humu2019Cover: "https://doi.org/10.1002/humu.23892",
+        ajhg2023: "https://doi.org/10.1016/j.ajhg.2022.12.001",
+        nature2020: "https://doi.org/10.1038/s41586-020-2832-5"
+    },
+    "External.resources": {
+        gnomad: "http://gnomad.broadinstitute.org/",
+        clinvar: "https://www.ncbi.nlm.nih.gov/clinvar/",
+        pfam: "https://www.ebi.ac.uk/interpro/entry/pfam/",
+        interpro: "https://www.ebi.ac.uk/interpro/",
+        gencode: "https://www.gencodegenes.org/",
+        uniprot: "http://www.uniprot.org/",
+        docker: "https://www.docker.com/",
+        bulma: "https://bulma.io/",
+        d3: "https://d3js.org/",
+        flask: "http://flask.pocoo.org/",
+        hmmer: "http://hmmer.org/",
+        postgresql: "https://www.postgresql.org/",
+        redis: "https://redis.io/",
+        celery: "https://docs.celeryq.dev/",
+        rabbitmq: "https://www.rabbitmq.com/",
+        chrome: "https://www.google.com/chrome/",
+        firefox: "https://www.mozilla.org/en-US/firefox/new/",
+        jalview: "http://www.jalview.org/",
+        ddd: "https://en.wikipedia.org/wiki/Domain-driven_design",
+        ucscConstraintTrack: "https://genome.ucsc.edu/cgi-bin/hgTrackUi?hgsid=1735422030_sAYmaQ0c58lMHr1JWvkyZ4gtGBGI&db=hg38&c=chr1&g=constraintSuper",
+        mobidetails: "https://mobidetails.iurc.montp.inserm.fr/MD/about",
+        opensourceMit: "http://opensource.org/licenses/mit-license.php",
+        ccByNcSa40: "http://creativecommons.org/licenses/by-nc-sa/4.0/"
+    },
+    "External.privacy": {
+        googlePrivacy: "https://policies.google.com/privacy",
+        googleTerms: "https://policies.google.com/terms",
+        gaIpPrivacy: "https://support.google.com/analytics/answer/12017362"
+    },
+    "External.affiliates": {
+        stanford: "https://www.stanford.edu/",
+        stanfordMedicine: "https://med.stanford.edu/",
+        radboudumcResearch: "https://www.radboudumc.nl/en/research",
+        radboudHumanGenetics: "https://www.radboudumc.nl/en/research/research-groups/genome-bioinformatics",
+        cmbi: "https://en.wikipedia.org/wiki/Center_for_Molecular_and_Biomolecular_Informatics"
+    }
+};
+
+function getStaticExternalLink(group, key) {
+    if (!isNonEmptyString(group) || !isNonEmptyString(key)) {
+        return null;
+    }
+
+    const groupLinks = STATIC_EXTERNAL_LINKS[group] || {};
+    return groupLinks[key] || null;
+}
+
+function applyStaticExternalLinks(root) {
+    const scope = root || document;
+
+    scope.querySelectorAll("[data-external-link-group][data-external-link-key]").forEach(function (element) {
+        const href = getStaticExternalLink(
+            element.dataset.externalLinkGroup,
+            element.dataset.externalLinkKey
+        );
+
+        if (href) {
+            element.href = href;
+        }
+    });
+}
+
 window.METADOME_LINKS = {
     ensemblTranscript(genomeBuild, transcriptId) {
         if (!isValidGenomeBuild(genomeBuild) || !isNonEmptyString(String(transcriptId || ""))) {
@@ -51,7 +131,7 @@ window.METADOME_LINKS = {
         const template = isGrch37(genomeBuild)
             ? "https://grch37.ensembl.org/Homo_sapiens/Transcript/Summary?t=__TRANSCRIPT__"
             : "https://ensembl.org/Homo_sapiens/Transcript/Summary?t=__TRANSCRIPT__";
-        return fillTemplate(template, { transcript_id: String(transcriptId).trim() });
+        return fillTemplate(template, {transcript_id: String(transcriptId).trim()});
     },
 
     ensemblGenomicPosition(genomeBuild, chromosome, position) {
@@ -62,7 +142,7 @@ window.METADOME_LINKS = {
         const template = isGrch37(genomeBuild)
             ? "https://grch37.ensembl.org/Homo_sapiens/Location/View?db=core;r=__CHR__:__POS__-__POS__"
             : "https://ensembl.org/Homo_sapiens/Location/View?r=__CHR__:__POS__-__POS__";
-        return fillTemplate(template, { chromosome: chr, position: Number(position) });
+        return fillTemplate(template, {chromosome: chr, position: Number(position)});
     },
 
     gnomadVariant(genomeBuild, chromosome, position, ref, alt) {
@@ -75,7 +155,7 @@ window.METADOME_LINKS = {
         const template = isGrch37(genomeBuild)
             ? "https://gnomad.broadinstitute.org/variant/__CHR__-__POS__-__REF__-__ALT__?dataset=gnomad_r2_1"
             : "https://gnomad.broadinstitute.org/variant/__CHR__-__POS__-__REF__-__ALT__?dataset=gnomad_r4";
-        return fillTemplate(template, { chromosome: chr, position: Number(position), ref: ref, alt: alt });
+        return fillTemplate(template, {chromosome: chr, position: Number(position), ref: ref, alt: alt});
     },
 
     clinvarVariant(clinvarId) {
@@ -114,44 +194,15 @@ window.METADOME_LINKS = {
         });
     },
 
-    about(key) {
-        const links = {
-            stanford: "https://www.stanford.edu/",
-            stanfordMedicine: "https://med.stanford.edu/",
-            radboudumcResearch: "https://www.radboudumc.nl/en/research",
-            radboudHumanGenetics: "https://www.radboudumc.nl/en/research/research-groups/genome-bioinformatics",
-            cmbi: "https://en.wikipedia.org/wiki/Center_for_Molecular_and_Biomolecular_Informatics",
-            metadomeApp: "https://www.metadome.app",
-            github: "https://github.com/laurensvdwiel/metadome",
-            githubReleases: "https://github.com/cmbi/laurensvdwiel/releases",
-            zenodo_metadome1: "https://zenodo.org/records/6625251",
-            zenodo_metadome2: "https://zenodo.org/records/19376150",
-            humu2019: "https://doi.org/10.1002/humu.23798",
-            humu2017: "https://doi.org/10.1002/humu.23313",
-            ajhg2023: "https://doi.org/10.1016/j.ajhg.2022.12.001",
-            nature2020: "https://doi.org/10.1038/s41586-020-2832-5",
-            humu2019Cover: "https://doi.org/10.1002/humu.23892",
-            ucscConstraintTrack: "https://genome.ucsc.edu/cgi-bin/hgTrackUi?hgsid=1735422030_sAYmaQ0c58lMHr1JWvkyZ4gtGBGI&db=hg38&c=chr1&g=constraintSuper",
-            mobidetails: "https://mobidetails.iurc.montp.inserm.fr/MD/about",
-            opensourceMit: "http://opensource.org/licenses/mit-license.php",
-            ccByNcSa40: "http://creativecommons.org/licenses/by-nc-sa/4.0/",
-            googleAnalytics: "https://marketingplatform.google.com/about/analytics/",
-            gencode: "https://www.gencodegenes.org/",
-            interpro: "https://www.ebi.ac.uk/interpro/",
-            hmmer: "http://hmmer.org/",
-            flask: "http://flask.pocoo.org/",
-            postgresql: "https://www.postgresql.org/",
-            redis: "https://redis.io/",
-            celery: "https://docs.celeryq.dev/",
-            rabbitmq: "https://www.rabbitmq.com/",
-            docker: "https://www.docker.com/",
-            bulma: "https://bulma.io/",
-            d3: "https://d3js.org/",
-            uniprotSite: "http://www.uniprot.org/",
-            pfamSite: "http://pfam.xfam.org/",
-            gnomadSite: "http://gnomad.broadinstitute.org/",
-            clinvarSite: "https://www.ncbi.nlm.nih.gov/clinvar/"
-        };
-        return links[key] || null;
+    get(group, key) {
+        return getStaticExternalLink(group, key);
+    },
+
+    apply(root) {
+        applyStaticExternalLinks(root);
     }
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+    applyStaticExternalLinks(document);
+});
